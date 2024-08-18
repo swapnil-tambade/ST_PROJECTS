cb6bc4332e79e91a6c89a2e03bd24e3565725677
swapnil-tambade <swapnil_tambade@colpal.com>
2024-08-19 00:04:35 +0530

    lecture 2 commit end

---
+++ Python learnings/lecture2.py
@@ -0,0 +1,55 @@
+# str1="SWAPNIL"
+# str2="TAMBADE"
+
+# print(str1+str2)  #concatenates two strings
+
+# print(len(str1))   #calculates the length of string
+
+
+# -------------------------------------
+
+
+# var1=input("My name is : ")
+# print("Length of my name is : ",len(var1))
+
+
+# ---------conditional statements-----------------------------
+
+# age=int(input("My age is : "))
+
+
+# if(age>=18):
+#     print('eligible for voting')
+# else :
+#     print('not eligible for voting')
+
+
+
+
+# light=input("Light color is  : ")
+
+
+# if(light=='Yellow'):
+#     print('slow down')
+# elif (light== 'Green'):
+#     print('you can GO')
+# elif (light=='Red'):
+#     print('please STOP')
+# else:
+#     print('Light ERROR')        
+
+
+
+#--------------number if Even or odd-----------
+
+num=float(input('typer your number: '))
+
+rem=num%2
+
+
+if(rem==0):
+    print('number is EVEN')
+else:
+    print('number is ODD')
+
+
