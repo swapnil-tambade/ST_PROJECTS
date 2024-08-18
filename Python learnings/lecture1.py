b12ac9ee4193f068b996169f6610176b30b4b011
swapnil-tambade <swapnil_tambade@colpal.com>
2024-08-18 23:06:47 +0530

    lecture 1 ST commit

---
+++ Python learnings/lecture1.py
@@ -0,0 +1,22 @@
+# print("Hello Swapnil")
+# #------------------------------------
+# num1=8
+# num2=10
+# print(num1+num2)
+
+#-------------normal input method------------
+# var1=input("enter first number: ")
+
+# var2=input("enter second number: ")
+
+# print(var1+var2)
+
+#----------------sum of two numbers via input method-----------------
+
+# var1=int(input("enter first number: "))
+
+# var2=int(input("enter second number: "))
+
+# print(var1+var2)
+
+
