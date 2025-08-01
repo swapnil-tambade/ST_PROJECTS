cb2763a56c18820b63d311f7ff56ac0fc541f7a1
Swap Tam <swaptam.sf1@gmail.com>
2025-08-01 02:15:31 +0000

    cur and con model

---
+++ models/customer_rev/customer_revenue.sql
@@ -0,0 +1,19 @@
+{{ config(
+    database='SLEEKMART_OMS',
+    schema='CUST_REV_CON',
+    materialized='table'
+) }}
+
+
+SELECT
+    OS.CustomerID,
+    C.CustomerName,
+    SUM(OS.OrderCount) AS OrderCount,
+    SUM(OS.Revenue) AS Revenue
+FROM
+    {{ ref('orders_fact') }} OS
+JOIN
+    {{ ref('customers_stg') }} C ON OS.CustomerID = C.CustomerID
+GROUP BY
+    OS.CustomerID,
+    C.CustomerName
