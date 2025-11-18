f08b47910f25cd60d9cd6f741e81679d6397421f
Swapnil Tambade <47742006+swapnil-tambade@users.noreply.github.com>
2025-11-18 13:32:49 -0500

    Update stg_sales_orders.sql

--- Project1_Retail_Sales_Analytics/dbt/models/staging/stg_sales_orders.sql
+++ Project1_Retail_Sales_Analytics/dbt/models/staging/stg_sales_orders.sql
@@ -1,1 +1,16 @@
+{{ config(materialized='view') }}
 
+  
+with raw as (
+select
+order_id,
+to_timestamp(order_date) as order_date,
+customer_id,
+item_id,
+quantity::INTEGER as quantity,
+unit_price::FLOAT as unit_price,
+currency,
+region
+from {{ source('raw','orders_raw') }}
+)
+select * from raw;
