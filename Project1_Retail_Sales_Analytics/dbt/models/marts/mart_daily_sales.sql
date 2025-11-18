9d18236dad8803803617ecd3a3efc77e079a7453
Swapnil Tambade <47742006+swapnil-tambade@users.noreply.github.com>
2025-11-18 13:36:06 -0500

    Update mart_daily_sales.sql

--- Project1_Retail_Sales_Analytics/dbt/models/marts/mart_daily_sales.sql
+++ Project1_Retail_Sales_Analytics/dbt/models/marts/mart_daily_sales.sql
@@ -1,1 +1,13 @@
+{{ config(
+  materialized='table'
+  ) }}
 
+---reading data from intermediate incremental table and calculating total sales and total qty
+
+  select
+date_trunc('day', order_date) as order_date,
+region,
+sum(sales_amount) as total_sales,
+sum(quantity) as total_quantity
+from {{ ref('int_sales_enriched') }}
+group by 1,2;
