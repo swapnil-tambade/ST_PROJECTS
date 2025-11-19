6400fbb93b7c7b1ad1102389e02d35f2914f4b48
Swapnil Tambade <47742006+swapnil-tambade@users.noreply.github.com>
2025-11-19 12:49:22 -0500

    Update create_pipes.sql

--- Project1_Retail_Sales_Analytics/snowflake/create_pipes.sql
+++ Project1_Retail_Sales_Analytics/snowflake/create_pipes.sql
@@ -1,5 +1,6 @@
 -- Snowpipe to load data from S3/GCS bucket to raw SF table(Replace notification settings as required.
 --external stage is already created with GCS/S3 BUCKET location (@raw.stage.raw_orders_stg)
+--with the help of snowpipe we are loading csv file data(located on external stage) into raw layer SF table
 
 CREATE OR REPLACE PIPE finance_db.raw.orders_data_pipe
 AUTO_INGEST = FALSE
