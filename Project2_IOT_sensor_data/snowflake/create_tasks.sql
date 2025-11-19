19b9382156bbcb38e36d9da512d718807ba78e86
Swapnil Tambade <47742006+swapnil-tambade@users.noreply.github.com>
2025-11-19 13:21:28 -0500

    Update create_tasks.sql

--- Project2_IOT_sensor_data/snowflake/create_tasks.sql
+++ Project2_IOT_sensor_data/snowflake/create_tasks.sql
@@ -1,8 +1,9 @@
 -- Task to move data from stream table into a processed table daily (or frequent schedule)
+--this task will only loading data from stream table to processed table to store recently loaded records
 
 CREATE OR REPLACE TASK finance_db.raw.process_iot_stream_task
 WAREHOUSE = COMPUTE_WH
-SCHEDULE = 'USING CRON * * * * * UTC' -- runs every minute (example - adjust as needed)
+SCHEDULE = 'USING CRON * 5 * * * UTC' -- runs every 5 minutes interval 
 AS
 INSERT INTO finance_db.raw.iot_sensor_events_processed (event_id, device_id, event_ts, temp_c, vibration)
 SELECT
