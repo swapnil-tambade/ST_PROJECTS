07b2627a6df7e950ec1633e988047a5f26bf4a39
Swapnil Tambade <47742006+swapnil-tambade@users.noreply.github.com>
2025-11-19 14:50:05 -0500

    Create dq_checks.sql

---
+++ Project3_Financial_Transactions_DQ/dbt/macros/dq_checks.sql
@@ -0,0 +1,10 @@
+{% macro assert_positive_amount(model, column) -%}
+select count(*) as failures
+from {{ model }}
+where {{ column }} <= 0 or {{ column }} is null
+{%- endmacro %}
+{% macro run_data_quality_checks() -%}
+-- This macro could be invoked from a task to run multiple checks and log
+results
+select
+'{{ this }}' as model_name,
