5645293d8cbee8cb3e857021f58e2527754cf181
Swapnil Tambade <47742006+swapnil-tambade@users.noreply.github.com>
2025-11-19 15:03:33 -0500

    Update dq_checks.sql

--- Project3_Financial_Transactions_DQ/dbt/macros/dq_checks.sql
+++ Project3_Financial_Transactions_DQ/dbt/macros/dq_checks.sql
@@ -1,10 +1,17 @@
+---below macro test will confirm if output generated for provided column of the output is positive or not
+--we ar checking number should be positive and not null
+
 {% macro assert_positive_amount(model, column) -%}
 select count(*) as failures
 from {{ model }}
 where {{ column }} <= 0 or {{ column }} is null
 {%- endmacro %}
+
+----------------below test will return number of rows output generated for provided model---
+---so we can run this as posthook or separate dbt test after our main dbt model run completes and we can trigger alert if this number of rows are out of expected range
+
+  
 {% macro run_data_quality_checks() -%}
--- This macro could be invoked from a task to run multiple checks and log
 results
 select
 '{{ this }}' as model_name,
