41c287a21c339590199de875e80d57be816a29d0
Swapnil Tambade <47742006+swapnil-tambade@users.noreply.github.com>
2025-11-19 11:54:28 -0500

    Update int_sensor_cleaned.sql

--- Project2_IOT_sensor_data/models/intermediate/int_sensor_cleaned.sql
+++ Project2_IOT_sensor_data/models/intermediate/int_sensor_cleaned.sql
@@ -1,1 +1,29 @@
+{{ config(
+  database='finance_db',
+  schema='intermediate',
+  materialized='incremental',
+  unique_key='event_id') }}
 
+----------------------------reading data from stating table in CTE--------------------
+with src as (
+select
+event_id,
+device_id,
+event_ts,
+data
+from {{ ref('stg_sensor_data') }}
+)
+
+-------------------------------------cleaning and casting as per project need-----------------------
+select
+event_id,
+device_id,
+to_timestamp(event_ts) as event_ts,
+try_cast(data:temperature::float as float) as temperature_c,
+try_cast(data:vibration::float as float) as vibration
+from src
+
+---------------------------------------writing incremental logic below--------------------------------
+{% if is_incremental() %}
+where event_ts > (select max(event_ts) from {{ this }})
+{% endif %}
