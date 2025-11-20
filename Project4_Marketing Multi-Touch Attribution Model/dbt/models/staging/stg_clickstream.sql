d189ef71e05dd4399966578b6a861ea2dbfc7888
Swapnil Tambade <47742006+swapnil-tambade@users.noreply.github.com>
2025-11-19 22:53:40 -0500

    Create stg_clickstream.sql

---
+++ Project4_Marketing Multi-Touch Attribution Model/dbt/models/staging/stg_clickstream.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='view') }}
+select
+cast(event_id as varchar) as event_id,
+user_id,
+to_timestamp(event_ts) as event_ts,
+page_url,
+channel
+from {{ source('raw','clickstream') }}
